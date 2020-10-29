1c88befcb861b20097050fb73c0fdfb762e2f702
SEKINE IKUTO <scaithikumin@gmail.com>
2020-10-29 09:11:58 +0900

    ピクチャを軽量化しました。

--- TeamProjectBase/Source Code/picture.h
+++ TeamProjectBase/Source Code/picture.h
@@ -35,7 +35,12 @@ public:
 		MASK_UPDATE = 0b0010,
 		MASK_MAX = 0b0011,
 	};
-
+	typedef enum
+	{
+		MODE_EDIT = 0,	// 編集
+		MODE_EXHI,		// 展示
+		MODE_MAX		// 最大数
+	} MODE;
 
 	/* メンバ関数 */
 	inline           CPicture() :CScene(){}																	// コンストラクタ
@@ -48,6 +53,7 @@ public:
 
 	static HRESULT   Load(void);																			// 読み込み
 	static void      InitStaticMember(void);																// 静的メンバの初期化
+	static void      UninitStaticMember(void);																// 静的メンバの終了
 
 	void             MatrixCal(void);																		// マトリックスの計算
 
@@ -56,6 +62,8 @@ public:
 	inline void      SetRot(CONST D3DXVECTOR3 &rot) { m_trans.rot = rot; }									// 向きの設定
 	inline void      SetScal(CONST D3DXVECTOR3 &scal) { m_trans.scal = scal; }								// スケールの設定
 	inline void      SetFlag(const int nMask) { m_Flags.cValue = nMask; }									// フラグの設定
+	inline void      SetFileName(CONST_STRING FileName) { m_FileName.Set(FileName); }						// ファイル名の設定
+	inline void      SetMode(MODE mode) {m_mode = mode; }													// モードの設定
 	inline void      BuildFlag(const int nFlagIndex) { Mybfunc_bit_set(m_Flags.cValue, nFlagIndex); }		// フラグを建てる
 	inline void      BreakFlag(const int nFlagIndex) { Mybfunc_bit_clear(m_Flags.cValue, nFlagIndex); }		// フラグを壊す
 
@@ -63,29 +71,38 @@ public:
 	inline UVSHORT*  GetFlag(void) { return &m_Flags.cValue; }												// フラグの取得
 
 	// クリエイト関数
-	static std::shared_ptr<CPicture> Create(CONST D3DXVECTOR3 &pos, CONST D3DXVECTOR3 &rot);				// 生成
+	static std::shared_ptr<CPicture>Create(CONST D3DXVECTOR3 &pos, CONST D3DXVECTOR3 &rot, CONST MODE mode = MODE_EDIT, CONST_STRING FileName = NULL);	// 生成
 private:
 	/* 内部で使う関数 */
-	void        MakeTexture(LPDIRECT3DDEVICE9 pDevice);					// テクスチャの作成
-	void        MakeVertex(LPDIRECT3DDEVICE9 pDevice);					// 頂点情報の作成
-	void        InitTextureColor(void);									// テクスチャカラーの初期化
+	void             MakeTexture(LPDIRECT3DDEVICE9 pDevice);					// テクスチャの作成
+	void             MakeVertex(LPDIRECT3DDEVICE9 pDevice);						// 頂点情報の作成
+	void             InitTextureColor(void);									// テクスチャカラーの初期化
+	static void      ReadFromLine(CONST_STRING Line);							// 1行から情報を読み取る
+	static void      CreatePixelPos(void);										// ピクセル位置の生成
+	static void      ReleasePixelPos(void);										// ピクセル位置の開放
+	static void      ReleasePen(void);											// ペンの開放
+	void             PaintProc(void);											// 塗る処理
+	bool             GetMousePosOnPicture(void);								// 絵上のマウス位置の取得
+	static void      Writing(void);												// 書き込み処理
+	static void      Reading(CString& file);									// 読み込み
+	static void      TexterReadFromLine(CONST_STRING cnpLine, CONST_STRING cnpEntryType, CONST_STRING cnpEntryData);// テクスチャ情報の1行から情報を読み取る
 
-	static void ReadFromLine(CONST_STRING Line);						// 1行から情報を読み取る
-
-	void        PaintProc(void);										// 塗る処理
-	bool        GetMousePosOnPicture(void);								// 絵上のマウス位置の取得
 	/* メンバ変数 */
-	static INTEGER2          m_nNumPolyBlock;			// ポリゴン数(ずらす大きさの設定後2倍にする)
+	static INTEGER2          m_nNumPixelBlock;			// ピクセル数
 	static FLOAT2            m_size;					// 大きさ
 	static D3DXVECTOR3       m_PlaneNor;				// 平面の法線
 	static D3DXVECTOR2       m_PixelSize;				// ピクセルサイズ
 	static UINT              m_nNumDataMax;				// 最大データ数
 	static CPaintingPen*     m_pPen;					// ペンのポインタ
 	static D3DXVECTOR2       m_PixelSizehalf;			// ピクセルサイズの半分
+	static D3DXVECTOR2*      m_pPixelPos;				// ピクセル位置のポインタ
+	static UINT              m_nNumMakeFile;			// ファイルを作った回数
 	LPDIRECT3DVERTEXBUFFER9  m_pVtexBuff;				// 頂点バッファのポインタ
 	LPDIRECT3DTEXTURE9       m_pTexture;				// テクスチャポインタ
 	UBITS_4                  m_Flags;					// フラグ
 	TRANSFORM                m_trans;					// トランス情報
+	MODE                     m_mode;					// モード
+	CString                  m_FileName;				// ファイル名
 };
 
 
